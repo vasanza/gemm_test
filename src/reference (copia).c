f023f1e6a3c61ade734daf36a540066f6231cbb8
vasanza <vasanza@gmail.com>
2024-06-19 16:43:40 +0200

    update

--- src/reference (copia).c
+++ src/reference (copia).c
@@ -2,35 +2,26 @@
 #include <stdio.h>
 #include <math.h>
 
-
-#include "sdv_tracing.h"
-
 double ComputeDelta(int N, int M, double * next, double * prev){
 	double global_delta = 0.0;
-	trace_event(1000,4);
 	for(int i=0; i<N; ++i){
 		for(int j=0; j<M; ++j){
 			double delta = next[i*M + j] - prev[i*M + j];
 			global_delta += (delta*delta);
 		}
 	}
-	trace_event(1000,0);
 	return global_delta;
 }
 
-
 void Step(int N, int M, double * volumes, double * pressures, double * temperatures, double * new_temperatures, int BLOCK_DIM_X, int BLOCK_DIM_Y, int * bounds){
 
-	trace_event(1000,1);
 	for(int i=0; i<N; ++i){
 		for(int j=0; j<M; ++j){
 			double length = (volumes[i*M+j]>1.0) ? cbrt(volumes[i*M+j]) : 0.5; 
 			pressures[i*M+j] = length + (temperatures[i*M+j]-new_temperatures[i*M+j]);
 		}
 	}
-	trace_event(1000,0);
 
-	trace_event(1000,2);
 	for(int block_i=1; block_i<N-BLOCK_DIM_Y; block_i+=BLOCK_DIM_Y){
 		for(int block_j=1; block_j<M-BLOCK_DIM_X; block_j+=BLOCK_DIM_X){
 			for(int i=block_i; i<block_i+BLOCK_DIM_Y; ++i){
@@ -43,15 +34,12 @@ void Step(int N, int M, double * volumes, double * pressures, double * temperatu
 			}
 		}
 	}
-	trace_event(1000,0);
 
-	trace_event(1000,3);
 	for(int i=0; i<N; ++i){
 		for(int j=0; j<M; ++j){
 			volumes[i*M+j] = pressures[bounds[i*M+j]] * new_temperatures[i*M+j];
 		}
 	}
-	trace_event(1000,0);
 }
 
 void initialize_2d_rand(int N, int M, double * array){
@@ -99,10 +87,6 @@ int main(){
 	double * pressures = (double *)malloc(sizeof(double) * N*M);
 	int * bounds = (int *)malloc(sizeof(int)*N*M);
 
-	initialize_tracing();
-	define_regions(1000,"Pressures","Temperatures","Volumes","Delta");
-	disable_tracing();
-
 	srand(0);
 	initialize_2d_rand(N,M,volumes);
 	initialize_2d_rand(N,M,temperatures);
@@ -113,11 +97,9 @@ int main(){
 	double MicrosPerStep=0.0;
 	int timestep;
 	for(timestep=0; timestep<10; ++timestep){
-		if (timestep==0) enable_tracing();
 		double t1 = GetMicros();
 		Step(N, M, volumes, pressures, temperatures, new_temperatures, bx, by, bounds);
 		double delta = ComputeDelta(N,M,new_temperatures,temperatures);
-		disable_tracing();
 		MicrosPerStep += GetMicros() - t1;
 		printf("%.4f\n",delta);
 		if (delta < 1.0) break;
--- src/reference.c
+++ src/reference.c
@@ -1,112 +1,210 @@
 #include <stdlib.h>
-#include <stdio.h>
 #include <math.h>
-
-double ComputeDelta(int N, int M, double * next, double * prev){
-	double global_delta = 0.0;
-	for(int i=0; i<N; ++i){
-		for(int j=0; j<M; ++j){
-			double delta = next[i*M + j] - prev[i*M + j];
-			global_delta += (delta*delta);
-		}
-	}
-	return global_delta;
-}
-
-void Step(int N, int M, double * volumes, double * pressures, double * temperatures, double * new_temperatures, int BLOCK_DIM_X, int BLOCK_DIM_Y, int * bounds){
-
-	for(int i=0; i<N; ++i){
-		for(int j=0; j<M; ++j){
-			double length = (volumes[i*M+j]>1.0) ? cbrt(volumes[i*M+j]) : 0.5; 
-			pressures[i*M+j] = length + (temperatures[i*M+j]-new_temperatures[i*M+j]);
-		}
-	}
-
-	for(int block_i=1; block_i<N-BLOCK_DIM_Y; block_i+=BLOCK_DIM_Y){
-		for(int block_j=1; block_j<M-BLOCK_DIM_X; block_j+=BLOCK_DIM_X){
-			for(int i=block_i; i<block_i+BLOCK_DIM_Y; ++i){
-				for(int j=block_j; j<block_j+BLOCK_DIM_X; ++j){
-					new_temperatures[i*M + j] = 0.25*(temperatures[M*i + j + 1]
-					                                + temperatures[M*(i+1) + j]
-																					+ temperatures[M*i + (j-1)]
-																					+ temperatures[M*(i-1) + j]);
-				}
-			}
-		}
-	}
-
-	for(int i=0; i<N; ++i){
-		for(int j=0; j<M; ++j){
-			volumes[i*M+j] = pressures[bounds[i*M+j]] * new_temperatures[i*M+j];
-		}
-	}
-}
-
-void initialize_2d_rand(int N, int M, double * array){
-	for(int i=0; i<N; ++i){
-		for(int j=0; j<M; ++j){
-			array[i*M + j] = (rand()%1024) / 1024.0;
-		}
-	}
-}
-void initialize_2d_zero(int N, int M, double * array){
-	for(int i=0; i<N; ++i){
-		for(int j=0; j<M; ++j){
-			array[i*M + j] = 0; 
-		}
-	}
-}
-void initialize_indexes(int N, int M, int * indexes){
-	for(int i=0; i<N; ++i){
-		for(int j=0; j<M; ++j){
-			indexes[i*M + j] = (((i+1) * 92401) ^ ((j+1) * 100003) ^ ((i+1)*(j+1) * 21031))%(N*M);
-		}
-	}
-}
-
 #include <sys/time.h>
 #include <stdio.h>
  
-double GetMicros(){ 
-	  struct timeval tv;
-	  gettimeofday(&tv, NULL);
-		return (double)tv.tv_sec*1e6 + (double)tv.tv_usec;
+#include "bl_sgemm.h"
+
+#define ERROR_TEST
+
+#define TOLERANCE 1E-2
+void computeError(
+        int    ldc,
+        int    ldc_ref,
+        int    m,
+        int    n,
+        float *C,
+        float *C_ref
+        )
+{
+    int    i, j;
+    for ( i = 0; i < m; i ++ ) {
+        for ( j = 0; j < n; j ++ ) {
+            if ( fabs( C( i, j ) - C_ref( i, j ) ) > TOLERANCE ) {
+                printf( "C[ %d ][ %d ] != C_ref, %E, %E\n", i, j, C( i, j ), C_ref( i, j ) );
+                break;
+            }
+        }
+    }
+
 }
 
-int main(){
+void test_bl_sgemm(
+        FILE *fp,
+        int m,
+        int n,
+        int k
+        ) 
+{
+    int    i, j, p, nx;
+    float *A, *B, *C, *C_ref;
+    float tmp, error, flops;
+    float ref_beg, ref_time, bl_sgemm_beg, bl_sgemm_time;
+    int    nrepeats;
+    int    lda, ldb, ldc, ldc_ref;
+    float ref_rectime, bl_sgemm_rectime;
 
-	int N=320 +2;
-	int M=160 +2;
+    A    = (float*)malloc( sizeof(float) * m * k );
+    B    = (float*)malloc( sizeof(float) * k * n );
 
-	int by=32;
-	int bx=32;
+    lda = m;
+    ldb = k;
+    ldc     = m;
+    ldc_ref = m;
 
-	double * volumes = (double *)malloc(sizeof(double) * N*M);
-	double * temperatures = (double *)malloc(sizeof(double) * N*M);
-	double * new_temperatures = (double *)malloc(sizeof(double) * N*M);
-	double * pressures = (double *)malloc(sizeof(double) * N*M);
-	int * bounds = (int *)malloc(sizeof(int)*N*M);
+    C     = bl_malloc_aligned( ldc, n + 4, sizeof(float) );
+    // Propósito: La función bl_malloc_aligned asigna un bloque de memoria alineado adecuadamente para operaciones de alta eficiencia.
+    // Alineación: Alinear la memoria puede ser crucial para aprovechar ciertas optimizaciones de hardware, especialmente en operaciones con vectores (SIMD) y matrices.
+    // Es la dimensión principal o "leading dimension" de la matriz C. En este caso, ldc es igual a m (el número de filas de la matriz C).
+    // Esta es la cantidad de columnas a reservar para la matriz C. Se está reservando espacio para n columnas más un adicional de 4 columnas.
+    //Este extra podría ser para asegurar un alineamiento correcto en memoria o para evitar problemas de desbordamiento de memoria.
+    
+    C_ref = (float*)malloc( sizeof(float) * m * n );
 
-	srand(0);
-	initialize_2d_rand(N,M,volumes);
-	initialize_2d_rand(N,M,temperatures);
-	initialize_2d_zero(N,M,new_temperatures);
-	initialize_2d_zero(N,M,pressures);
-	initialize_indexes(N,M,bounds);
+    nrepeats = 5; //------------ 3
+    // Medición del Rendimiento: Repetir las operaciones varias veces ayuda a obtener una medida más precisa del rendimiento.
+    // Las mediciones pueden variar debido a factores como la carga del sistema, caché hits/misses, etc. 
+    // Al repetir la operación y tomar el tiempo más bajo (bl_sgemm_rectime y ref_rectime), se intenta obtener una estimación más fiable del rendimiento óptimo.
+    // Promedio o Mejor Tiempo: El código actual toma el menor tiempo registrado en las repeticiones para cada operación (bl_sgemm y bl_sgemm_ref).
+    // Esto se hace para minimizar la influencia de cualquier anomalía temporal que pueda ocurrir durante una de las ejecuciones (como un breve aumento en la carga del sistema).
 
-	double MicrosPerStep=0.0;
-	int timestep;
-	for(timestep=0; timestep<10; ++timestep){
-		double t1 = GetMicros();
-		Step(N, M, volumes, pressures, temperatures, new_temperatures, bx, by, bounds);
-		double delta = ComputeDelta(N,M,new_temperatures,temperatures);
-		MicrosPerStep += GetMicros() - t1;
-		printf("%.4f\n",delta);
-		if (delta < 1.0) break;
+    srand48 (time(NULL));
 
-		double * tmp = temperatures;
-		temperatures = new_temperatures;
-		new_temperatures = tmp;
-	}
-	printf("Microseconds per step: %.2f\n", MicrosPerStep/timestep);
+    // Randonly generate points in [ 0, 1 ].
+    for ( p = 0; p < k; p ++ ) {
+        for ( i = 0; i < m; i ++ ) {
+            A( i, p ) = (float)( drand48() );	
+        }
+    }
+    for ( j = 0; j < n; j ++ ) {
+        for ( p = 0; p < k; p ++ ) {
+            B( p, j ) = (float)( drand48() );
+        }
+    }
+
+    for ( j = 0; j < n; j ++ ) {
+        for ( i = 0; i < m; i ++ ) {
+            C_ref( i, j ) = (float)( 0.0 );	
+                C( i, j ) = (float)( 0.0 );	
+        }
+    }
+
+    for ( i = 0; i < nrepeats; i ++ ) {
+        bl_sgemm_beg = bl_clock();
+        {
+            bl_sgemm(
+                    m,
+                    n,
+                    k,
+                    A,
+                    lda,
+                    B,
+                    ldb,
+                    C,
+                    ldc
+                    );
+        }
+        bl_sgemm_time = bl_clock() - bl_sgemm_beg;
+
+        if ( i == 0 ) {
+            bl_sgemm_rectime = bl_sgemm_time;
+        } else {
+            bl_sgemm_rectime = bl_sgemm_time < bl_sgemm_rectime ? bl_sgemm_time : bl_sgemm_rectime;
+        }
+    }
+
+#ifdef ERROR_TEST
+    for ( i = 0; i < nrepeats; i ++ ) {
+        ref_beg = bl_clock();
+        {
+            bl_sgemm_ref(
+                    m,
+                    n,
+                    k,
+                    A,
+                    lda,
+                    B,
+                    ldb,
+                    C_ref,
+                    ldc_ref
+                    );
+        }
+        ref_time = bl_clock() - ref_beg;
+
+        if ( i == 0 ) {
+            ref_rectime = ref_time;
+        } else {
+            ref_rectime = ref_time < ref_rectime ? ref_time : ref_rectime;
+        }
+    }
+
+    computeError(
+            ldc,
+            ldc_ref,
+            m,
+            n,
+            C,
+            C_ref
+            );
+#endif
+
+    // Compute overall floating point operations.
+    flops = ( m * n / ( 1000.0 * 1000.0 * 1000.0 ) ) * ( 2 * k );
+    
+    // <-------------------------------------------------------------------------printf results real time
+    //printf( "%5d\t %5d\t %5d\t %5.3lf\t %5.3lf\n", 
+    //        m, n, k, flops / bl_sgemm_rectime, flops / ref_rectime );
+    
+    // Guardar los resultados en el archivo CSV.
+    fprintf(fp, "%d,%d,%d,%5.3lf,%5.3lf\n", m, n, k, flops / bl_sgemm_rectime, flops / ref_rectime);
+
+
+    free( A     );
+    free( B     );
+    free( C     );
+    free( C_ref );
 }
+
+int main( int argc, char *argv[] )
+{
+    //printf("%%m\t%%n\t%%k\t%%MY_GFLOPS\t%%REF_GFLOPS\n");
+    //for(int i = 16; i <= 800; i += 4) {
+    //    test_bl_sgemm( i, i, i );
+    //}
+
+    //return 0;
+
+    FILE *fp = fopen("results.csv", "w");
+    if (!fp) {
+        perror("No se pudo abrir el archivo CSV");
+        return 1;
+    }
+
+    // Escribir encabezado en el archivo CSV.
+    fprintf(fp, "m,n,k,Version0,Version1\n"); //<----------------------------Set actual version
+
+    //printf("%%m\t%%n\t%%k\t%%Version0\t%%Version1\n");
+    printf("Start\n");
+    //<--------------------------------------------------------------------------------
+    //for(int i = 16; i <= 800; i += 4) {
+    
+    // Prueba 1: A(m=4k,K=4k) * B(K=4k,n=4k) = C(m=4k,n=4k)
+    //for (int j = 10; j <= 800; j+= 100){
+        //for(int i = 16; i <= 800; i += 4) {
+        for(int i = 20; i < 1000; i += 20) {//<----------------------------------Set max number of iterations and step betwen interations
+            test_bl_sgemm(fp,i, i, i);
+        }
+    //}
+
+    // Prueba 2: A(m=4k,K=4k) * B(K=4k,n=11k) = C(m=4k,n=11k)
+    //for(int i = 16; i <= 4000; i += 4) {
+    //    test_bl_sgemm(fp,i, i, 11000);
+    //}
+
+    fclose(fp);
+    printf("ok\n");
+
+    return 0;
+}
+
+
